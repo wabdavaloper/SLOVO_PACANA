33419a80782c56e737bd09172b7eaafb5aaa48f6
Narutsky-Amosov Mikhail <mazay62rus@gmail.com>
2023-12-06 11:58:24 +0300

    1

--- NewToken.sol
+++ NewToken.sol
@@ -1,13 +1,13 @@
-pragma solidity ^0.5.7;
+pragma solidity ^0.8.18;
 
 import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	constructor() public {
-		totalSupply = 123;
-		name = "Best ever coin";
+		totalSupply = 1000000;
+		name = "SLOVO PACANA COIN";
 		decimals = 4;
-		symbol = "BEC";
+		symbol = "SPC";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
